4a63e80999474ff9ed4f078d54f308542130c4f2
SimoYang <30398003+SimoYang@users.noreply.github.com>
2017-10-21 14:26:41 +0800

    description
    重新编码了注释

--- src/robot/OperateProcess.java
+++ src/robot/OperateProcess.java
@@ -11,6 +11,7 @@ import ToObjectUtil.ProcessBean;
 public class OperateProcess {
 		Robot r;
 		static Map<String,Integer> key;
+		//创建一个包含所有KeyCode的Map
 		static{
 		    key=new HashMap<>();
 			key.put("SHIFT", KeyEvent.VK_SHIFT);
@@ -18,7 +19,44 @@ public class OperateProcess {
 			key.put("TAB",KeyEvent.VK_TAB);
 			key.put("ENTER",KeyEvent.VK_ENTER);
 			key.put("A",KeyEvent.VK_A);
+			key.put("B", KeyEvent.VK_B);
+			key.put("C", KeyEvent.VK_C);
+			key.put("D", KeyEvent.VK_D);
+			key.put("E", KeyEvent.VK_E);
+			key.put("G", KeyEvent.VK_G);
+			key.put("H", KeyEvent.VK_H);
+			key.put("I", KeyEvent.VK_I);
+			key.put("J", KeyEvent.VK_J);
+			key.put("K", KeyEvent.VK_K);
+			key.put("L", KeyEvent.VK_L);
+			key.put("M", KeyEvent.VK_M);
+			key.put("N", KeyEvent.VK_N);
+			key.put("O", KeyEvent.VK_O);
+			key.put("P", KeyEvent.VK_P);
+			key.put("Q", KeyEvent.VK_Q);
+			key.put("R", KeyEvent.VK_R);
+			key.put("S", KeyEvent.VK_S);
+			key.put("T", KeyEvent.VK_T);
+			key.put("U", KeyEvent.VK_U);
+			key.put("V", KeyEvent.VK_V);
+			key.put("W", KeyEvent.VK_W);
+			key.put("X", KeyEvent.VK_X);
+			key.put("Y", KeyEvent.VK_Y);
+			key.put("Z", KeyEvent.VK_Z);
+			key.put("ALT", KeyEvent.ALT_MASK);
+			key.put("WINDOW", KeyEvent.VK_WINDOWS);
+			key.put("BACKS", KeyEvent.VK_BACK_SPACE);
+			key.put("1", KeyEvent.VK_1);
+			key.put("2", KeyEvent.VK_2);
+			key.put("3", KeyEvent.VK_3);
+			key.put("4", KeyEvent.VK_4);
+			key.put("5", KeyEvent.VK_5);
+			key.put("6", KeyEvent.VK_6);
+			key.put("7", KeyEvent.VK_7);
+			key.put("8", KeyEvent.VK_8);
+			key.put("9", KeyEvent.VK_9);
 		}
+		//初始化OperateProcess
 		public OperateProcess(){
 			try {
 				r=new Robot();
@@ -27,9 +65,11 @@ public class OperateProcess {
 				e.printStackTrace();
 			}
 		}
+		//处理键盘输入的接口
 		public void OperateKeyP(String s){
 			int s1=key.get(s);
 			r.keyPress(s1);
 		}
+		//
 		
 }
